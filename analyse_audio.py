30eb8ca51d6424f97aff1b7988f33e33cd6f72a9
Mike Cullimore <michael.cullimore11@gmail.com>
2023-10-15 20:33:07 +0100

    Start analysis of music file.

---
+++ analyse_audio.py
@@ -0,0 +1,16 @@
+from utils import constant_q_transform, load, specshow, waveshow
+
+
+def main():
+    filename = "01 Toccata and Fugue for organ in D minor, BWV 565.mp3"
+    # TODO: pass sampling_frequency to subsequent functions (can't assume 44kHz!).
+    signal, sampling_frequency = load(filename)
+
+    waveshow(signal)
+
+    C = constant_q_transform(signal)
+    specshow(C)
+
+
+if __name__ == "__main__":
+    main()
